5c7f201eba2d84ed1553f5e0217b26d3204d0fd6
dronraj <dronrajdas@gmail.com>
2024-07-20 22:27:13 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.title("Welcome to the page")
+st.header("DEMO")
+st.subheader("dummy")
+
+name = st.text_input ("Enter Your Name : ")
+fname = st.text_input("Enter Your Father Name : ")
+adr = st.text_area("Enter Your Address : ")
+classdata = st.selectbox("Enter Your Class :",(0,8,9,10,11,12))
+
+button = st.button("Done")
+if button :
+    st.markdown(f"""
+    Name : {name}
+    Father Nmae : {fname}
+    address : {adr}
+    class : {classdata}""")
+
+    
